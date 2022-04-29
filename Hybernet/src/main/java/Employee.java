3fd66b688d3622efd61ec66b2a471b11fe8e4ac0
Manojkumar R <2005632@haeindia.net>
2022-04-29 09:40:45 +0530

    J2EE JAVA

---
+++ Hybernet/src/main/java/Employee.java
@@ -0,0 +1,24 @@
+
+public class Employee {
+	private int id;
+	private String firstname,Secondname;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public  String getfirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firsrname) {
+		this.firstname = firstname;
+	}
+	public String getSecondname() {
+		return Secondname;
+	}
+	public void setSecondname() {
+		this.Secondname = Secondname;
+	}
+}
